c955a308beabb22ebb5b55ee22e3a7451f78e5fa
Fat Sin <sonicfbis@hotmail.com>
2017-10-04 23:27:44 +0200

    Ajout des images et de l'affichage graphique du niveau

--- gethimout.py
+++ gethimout.py
@@ -13,6 +13,47 @@ def create_level():
 	print("Voici le labyrinthe sous forme de liste:",level)
 	print("Voici la 4e ligne:",level[3])
 	return level
-	
-create_level()
+
+def display_level(level):
+        window = pg.display.set_mode((640,480), RESIZABLE)
+        keep_open = 1
+        
+        wall = pg.image.load("wall.png").convert()
+        macgyver = pg.image.load("macgyver.png").convert_alpha()
+        guardian = pg.image.load("guardian.png").convert_alpha()
+        num_line=0
+
+        for line in level :
+                num_col=0
+                y=num_line*480/15
+                for cell in line :
+                        x = num_col*640/15
+                        if cell=="w":
+                                window.blit(wall,(x,y))  
+                        if cell=="d":
+                                window.blit(macgyver,(x,y))
+                        if cell=="f":
+                                window.blit(guardian,(x,y))
+                        else:
+                                pass
+                        num_col += 1
+                num_line += 1
+           
+        pg.display.flip()      
+        while keep_open :
+                for event in pg.event.get(): 
+                        if event.type == QUIT :
+                                keep_open = 0
+                        elif event.type == KEYDOWN:
+                                if event.key == K_UP:
+                                        pass
+                                if event.key == K_DOWN:
+                                        pass
+                                if event.key == K_LEFT:
+                                        pass
+                                if event.key == K_RIGHT:
+                                        pass
+                
+current_level = create_level()
+display_level(current_level)
 
