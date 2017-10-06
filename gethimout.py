a31243b20d367f81d6fbc60cd22521d0ffaf0314
Fatou FS. SINDJEU <fsindjeu@alidaho.com>
2017-10-06 11:51:00 +0200

    Ajout de la classe Character avec méthodes d'animation du Macgyver.

--- gethimout.py
+++ gethimout.py
@@ -11,13 +11,13 @@ def create_level():
 				if cell != '\n':
 					lines.append(cell)
 			level.append(lines)
-	print("Voici le labyrinthe sous forme de liste:",level)
-	print("Voici la 4e ligne:",level[3])
+	#print("Voici le labyrinthe sous forme de liste:",level)
+	#print("Voici la 4e ligne:",level[3])
 	return level
 
-def display_level(level):
-        window = pg.display.set_mode((640,480), RESIZABLE)
-        keep_open = 1
+def display_level(window,level):
+        #window = pg.display.set_mode((640,480), RESIZABLE)
+        
         
         wall = pg.image.load("wall.png").convert()
         macgyver = pg.image.load("macgyver.png").convert_alpha()
@@ -44,12 +44,15 @@ def display_level(level):
                                 window.blit(wall,(x,y))  
                         if cell=="d":
                                 window.blit(macgyver,(x,y))
+                                position_macgyver = macgyver.get_rect()
+                                position_macgyver.center = (x,y)
                         if cell=="f":
                                 window.blit(guardian,(x,y))
                         elif cell=="0":
                                 empty_cells.append([x,y])                                
                         num_col += 1
                 num_line += 1
+        #position_macgyver = macgyver.get_rect() -> macgyver not centered here
         #Random positioning of the obects
         position1 = random.randint(0,len(empty_cells))
         position2 = random.randint(0,len(empty_cells))
@@ -60,22 +63,69 @@ def display_level(level):
         window.blit(needle,empty_cells[position1])
         window.blit(ether,empty_cells[position2])
         window.blit(tube,empty_cells[position3])
+        pg.display.flip()
+        elements = [macgyver,position_macgyver]
+        return elements
         
-        pg.display.flip()      
+current_level = create_level()
+
+current_window = pg.display.set_mode((640,480), RESIZABLE)
+
+#display_level(current_level)
+
+class Character():
+        def __init__(self, window, elements_level):
+                self.elements_level = elements_level
+                #self.position = position
+                self.window = window
+
+        
+        def move_char(self, direction):
+                #position = self.elements_level[1]
+                if direction == "up":
+                        #position = position.move(0,int(480/15))
+                        self.elements_level[1] = self.elements_level[1].move(0,-int(480/15))
+                        print(self.elements_level[1])
+                if direction == "down":
+                        #position = position.move(0,-int(480/15))
+                        self.elements_level[1] = self.elements_level[1].move(0,int(480/15))
+                        #print(position)                        
+                        print(self.elements_level[1])
+                if direction == "left":
+                        #position = position.move(-int(640/15),0)
+                        self.elements_level[1] = self.elements_level[1].move(-int(480/15),0)
+                        #print(position) 
+                        print(self.elements_level[1])
+                if direction == "right":
+                        #position = position.move(int(640/15),0)
+                        self.elements_level[1] = self.elements_level[1].move(int(480/15),0)
+                        #print(position) 
+                        print(self.elements_level[1])
+                self.window.blit(self.elements_level[0],self.elements_level[1])
+                pg.display.flip()
+                
+
+MacGyver = Character(current_window, display_level(current_window, current_level))
+
+def game_loop():
+        
+        keep_open = 1
         while keep_open :
                 for event in pg.event.get(): 
                         if event.type == QUIT :
                                 keep_open = 0
                         elif event.type == KEYDOWN:
                                 if event.key == K_UP:
-                                        pass
+                                        MacGyver.move_char("up")
                                 if event.key == K_DOWN:
-                                        pass
+                                        MacGyver.move_char("down")
                                 if event.key == K_LEFT:
-                                        pass
+                                        MacGyver.move_char("left")
                                 if event.key == K_RIGHT:
-                                        pass
-                
-current_level = create_level()
-display_level(current_level)
+                                        MacGyver.move_char("right")
+                                        
+                        pg.display.flip()
+                                
+
+game_loop()
 
